e10882f9747808fddf72d555ff99803b90431fa2
Antoine Pilette <antoine@oilinwater.be>
2015-11-03 17:27:00 +0100

    Stage All

--- oilinwater-team/wp-content/themes/oilinwater/functions.php
+++ oilinwater-team/wp-content/themes/oilinwater/functions.php
@@ -350,14 +350,14 @@ function html5blankcomments($comment, $args, $depth)
 \*------------------------------------*/
 
 // Add Actions
-add_action('init', 'html5blank_header_scripts'); // Add Custom Scripts to wp_head
-add_action('wp_print_scripts', 'html5blank_conditional_scripts'); // Add Conditional Page Scripts
-add_action('get_header', 'enable_threaded_comments'); // Enable Threaded Comments
-add_action('wp_enqueue_scripts', 'html5blank_styles'); // Add Theme Stylesheet
-add_action('init', 'register_html5_menu'); // Add HTML5 Blank Menu
-add_action('init', 'create_post_type_html5'); // Add our HTML5 Blank Custom Post Type
-add_action('widgets_init', 'my_remove_recent_comments_style'); // Remove inline Recent Comment Styles from wp_head()
-add_action('init', 'html5wp_pagination'); // Add our HTML5 Pagination
+// add_action('init', 'html5blank_header_scripts'); // Add Custom Scripts to wp_head
+// add_action('wp_print_scripts', 'html5blank_conditional_scripts'); // Add Conditional Page Scripts
+// add_action('get_header', 'enable_threaded_comments'); // Enable Threaded Comments
+// add_action('wp_enqueue_scripts', 'html5blank_styles'); // Add Theme Stylesheet
+// add_action('init', 'register_html5_menu'); // Add HTML5 Blank Menu
+// add_action('init', 'create_post_type_html5'); // Add our HTML5 Blank Custom Post Type
+// add_action('widgets_init', 'my_remove_recent_comments_style'); // Remove inline Recent Comment Styles from wp_head()
+// add_action('init', 'html5wp_pagination'); // Add our HTML5 Pagination
 
 // Remove Actions
 remove_action('wp_head', 'feed_links_extra', 3); // Display the links to the extra feeds such as category feeds
